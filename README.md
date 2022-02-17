1e24039aac93980e5b0d86f80554901287869104
Vasyl Vaskivskyi <vaskivskyi.v@gmail.com>
2022-02-17 17:02:28 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Internal standards for acquisition metadata
+
+HuBMAP internal metadata standards that describe microscopy acquisition details 
+for imaging spatial proteomics methods CODEX and ImmunoSABER.
---
+++ validate_meta.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import json
+import jsonschema
+
+cur_dir = Path().resolve()
+meta_dir = Path("metadata_standard/codex/1.0")
+exp_schema_path = cur_dir / meta_dir / "experiment_schema.json"
+with open(exp_schema_path, "r") as s:
+    exp_schema = json.load(s)
+
+exp_path = cur_dir / meta_dir / "experiment.json"
+with open(exp_path, "r") as s:
+    experiment = json.load(s)
+
+# res = None if successful
+res = jsonschema.validate(experiment, exp_schema)
