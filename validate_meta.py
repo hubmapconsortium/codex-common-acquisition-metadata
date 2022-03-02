a512d21b8d5a01fc58550503f2c8ad0cf1c86cc7
Vasyl Vaskivskyi <vaskivskyi.v@gmail.com>
2022-03-02 01:28:19 +0000

    fixed incorrect path to the test meta

--- validate_meta.py
+++ validate_meta.py
@@ -3,7 +3,7 @@ import json
 import jsonschema
 
 cur_dir = Path().resolve()
-meta_dir = Path("metadata_standard/codex/1.0")
+meta_dir = Path("metadata_standard/1.0")
 exp_schema_path = cur_dir / meta_dir / "experiment_schema.json"
 with open(exp_schema_path, "r") as s:
     exp_schema = json.load(s)
@@ -13,4 +13,4 @@ with open(exp_path, "r") as s:
     experiment = json.load(s)
 
 # res = None if successful
-res = jsonschema.validate(experiment, exp_schema)
+jsonschema.validate(experiment, exp_schema)
