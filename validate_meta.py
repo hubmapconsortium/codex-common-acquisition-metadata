ac6348d5db364c0efa374e50fd31e784b92a868f
nhpatterson <nheathpatterson@gmail.com>
2022-03-30 16:03:24 -0500

    Fix path to schema: experiment_schema.json --> dataset_schema.json

--- validate_meta.py
+++ validate_meta.py
@@ -4,11 +4,11 @@ import jsonschema
 
 cur_dir = Path().resolve()
 meta_dir = Path("metadata_standard/1.0")
-exp_schema_path = cur_dir / meta_dir / "experiment_schema.json"
+exp_schema_path = cur_dir / meta_dir / "dataset_schema.json"
 with open(exp_schema_path, "r") as s:
     exp_schema = json.load(s)
 
-exp_path = cur_dir / meta_dir / "experiment.json"
+exp_path = cur_dir / meta_dir / "dataset.json"
 with open(exp_path, "r") as s:
     experiment = json.load(s)
 
